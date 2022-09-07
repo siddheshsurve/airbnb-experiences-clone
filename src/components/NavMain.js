0c0e86eaf8f5dcd3005262962dd6913260f0632c
Siddhesh Surve <sysurve24@gmail.com>
2022-09-07 16:57:34 +0000

    Navbar done with styling

--- src/components/NavMain.js
+++ src/components/NavMain.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "../index.css"
+import Logo from "../images/logo.png"
 
 export default function NavMain () {
     return (
         <>
             <nav>
-                
+                <img className="nav--logo" src={Logo} />
             </nav>
         </>
     )
