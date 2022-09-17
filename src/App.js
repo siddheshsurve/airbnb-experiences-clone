f0dd6c9197e8f8ca07a60b64c374cd559bf2f021
Siddhesh Surve <sysurve24@gmail.com>
2022-09-17 13:05:31 +0000

    updated data.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,26 @@ import NavMain from "./components/NavMain";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
 import KatieP from "./images/katie-zaferes.png"
-import Data from "./data.js"
-export default function App () {
-  return (
-    <>
-      < NavMain/>
-      {/* < Hero/> */}
-      <Card 
-          img = {KatieP}
-          rating = "5.0"
-          reviewCount = {6}
-          country = "USA"
-          title="Life Lessons with Katie Zaferes"
-          price={136}
-      />
-    </>
-  )
+import data from "./data.js"
+export default function App() {
+  // <Hero />
+const cards = data.map(item => {
+return (
+  <Card 
+      img={item.coverImg}
+      rating={item.stats.rating}
+      reviewCount={item.stats.reviewCount}
+      location={item.location}
+      title={item.title}
+      price={item.price}
+  />
+)
+})        
+
+return (
+<div>
+  <NavMain />
+  {cards}
+</div>
+)
 }
