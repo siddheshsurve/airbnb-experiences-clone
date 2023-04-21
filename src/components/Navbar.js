4ae4bf7f327a497958029073eaf9d25970442af1
Siddhesh Surve <sysurve24@gmail.com>
2023-04-21 06:59:06 +0000

    init commit + added Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from "../assets/airbnb-logo.png"
+
+function Navbar() {
+  return (
+    <nav>
+        <img src = {Logo} />
+    </nav>
+  )
+}
+
+export default Navbar
